0fb89481f42b4449ae6cba1c3688c63b1713b867
Mederic <mederic.dominici@gmail.com>
2024-10-28 16:48:41 +0200

    US1 - Test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,11 +1,12 @@
 import unittest
 
 from bowling import BowlingGame
-from bowling_error import BowlingError
 from frame import Frame
-
 
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_game_created(self):
+        f = Frame(1,5)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0))
